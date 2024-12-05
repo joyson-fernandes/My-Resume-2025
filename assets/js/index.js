2432b3e3a33ac05facc3057bffced4646add3879
joyson-fernandes <jf@joysontech.com>
2024-12-05 01:49:32 +0000

    added page view count

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,13 @@
-// JavaScript Code
 const counter = document.querySelector(".counter-number");
 async function updateCounter() {
-    let response = await fetch("https://flqm7ld4yqfpu6numq57jqncem0ggnpj.lambda-url.us-east-1.on.aws/");
-    let data = await response.json();
-    counter.innerHTML = ` Views: ${data}`;    
+    try {
+        let response = await fetch("https://flqm7ld4yqfpu6numq57jqncem0ggnpj.lambda-url.us-east-1.on.aws/");
+        let data = await response.json();
+        console.log(data);  // This will log the fetched data to the console
+        counter.innerHTML = `${data.views} Awesome people visited this page`;  // Update the counter with the custom message
+    } catch (error) {
+        console.error('Error fetching view count:', error);
+    }
 }
 
 updateCounter();
